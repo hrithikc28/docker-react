53413f4a3c9ea316520b8b1aaf7715e73b31ac2c
Hritik Chauhan <hrithikc28@gmail.com>
2021-03-01 20:57:27 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        donllafhds lfahlLearn React GG haanji naaji kyaji okji
+        I was changed in the feature branch
 
         </a>
       </header>
